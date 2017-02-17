1207c05ab533f26de34d8ca98287cb6fb0615536
Amshar Basheer <amsharb@gmail.com>
2017-02-17 17:02:30 -0500

    add input filter to time input when adding new rule

--- app/src/main/java/com/example/kyle/joulieapp/NewRuleActivity.java
+++ app/src/main/java/com/example/kyle/joulieapp/NewRuleActivity.java
@@ -5,13 +5,18 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputFilter;
+import android.text.Spanned;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 import com.example.kyle.joulieapp.Models.DummyContent;
 import com.example.kyle.joulieapp.Models.Rule;
 
 public class NewRuleActivity extends AppCompatActivity implements RuleFragment.OnListFragmentInteractionListener{
+
+    private EditText editTextTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +25,7 @@ public class NewRuleActivity extends AppCompatActivity implements RuleFragment.O
         ActionBar ab = getSupportActionBar();
         ab.setTitle("Create New Rule");
         ab.setDisplayHomeAsUpEnabled(true);
-
+        initTimeInput();
 
     }
 
@@ -43,6 +48,68 @@ public class NewRuleActivity extends AppCompatActivity implements RuleFragment.O
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initTimeInput(){
+        editTextTime = (EditText) findViewById(R.id.time_input);
+
+        //the following InputFilter related code was borrowed from
+        //http://stackoverflow.com/questions/13120947/how-to-restrict-to-input-time-for-edittext-in-android
+        InputFilter[] timeFilter = new InputFilter[1];
+
+        timeFilter[0]   = new InputFilter() {
+            public CharSequence filter(CharSequence source, int start, int end, Spanned dest,
+                                       int dstart, int dend) {
+
+
+                if (source.length() == 0) {
+                    return null;// deleting, keep original editing
+                }
+                String result = "";
+                result += dest.toString().substring(0, dstart);
+                result += source.toString().substring(start, end);
+                result += dest.toString().substring(dend, dest.length());
+
+                if (result.length() > 5) {
+                    return "";// do not allow this edit
+                }
+                boolean allowEdit = true;
+                char c;
+
+                if (result.length() > 0) {
+                    c = result.charAt(0);
+                    allowEdit &= (c >= '0' && c <= '2' && !(Character.isLetter(c)));
+                }
+                if (result.length() > 1) {
+                    //modified borrowed code here to fix bug that allowed
+                    //invalid inputs like 24 to 29 for hour
+                    char ch;
+                    c = result.charAt(1);
+                    ch = result.charAt(0);
+                    if (ch == '2'){
+                        allowEdit &= (c >= '0' && c <= '3' && !(Character.isLetter(c)));
+                    }
+                    else{
+                        allowEdit &= (c >= '0' && c <= '9' && !(Character.isLetter(c)));
+                    }
+                }
+                if (result.length() > 2) {
+                    c = result.charAt(2);
+                    allowEdit &= (c == ':'&&!(Character.isLetter(c)));
+                }
+                if (result.length() > 3) {
+                    c = result.charAt(3);
+                    allowEdit &= (c >= '0' && c <= '5' && !(Character.isLetter(c)));
+                }
+                if (result.length() > 4) {
+                    c = result.charAt(4);
+                    allowEdit &= (c >= '0' && c <= '9'&& !(Character.isLetter(c)));
+                }
+                return allowEdit ? null : "";
+            }
+        };
+
+        editTextTime.setFilters(timeFilter);
     }
 
 }
